140c864bec044354523b048177c7758d18f58ce2
Cedrick F. Campoto <cedrickcampoto@yahoo.com>
2022-01-10 13:40:14 +0800

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const path = require('path');
 const fs = require('fs');
 const key = fs.readFileSync(path.join(__dirname, 'cert/key.pem'));
 const cert = fs.readFileSync(path.join(__dirname,'cert/cert.pem'));
+// const https = require('https');
+// const express = require('express');
 const { PeerServer } = require('peer');
+const port = process.env.PORT || 7422;
+
 
 PeerServer({
     path:'/peerjs',
-    port: 7422,
+    port,
     ssl:{
         key, cert,
     }
